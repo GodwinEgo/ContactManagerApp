ffaa2c778614c98f58c5169034b1b62db3c5de23
GodwinEgo <godwinehile04@gmail.com>
2023-08-02 23:42:01 +0100

    const

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -18,18 +18,25 @@ const errorHandler = (err, req, res, next) => {
       });
     case constants.UNAUTHORIZED:
       res.json({
-        title: "Validation Failed",
+        title: "Unauthorized",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
     case constants.FORBIDDEN:
       res.json({
-        title: "Not Found",
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "Server Errror",
         message: err.message,
         stackTrace: err.stack,
       });
     default:
+      console.log("No error, All Good");
       break;
   }
   res.json({ title: "Not Found", message: err.message, stackTrace: err.stack });
