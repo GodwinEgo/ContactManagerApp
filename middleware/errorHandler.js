5c612fa8295268e67c2487f18972ebab9d9a39bd
GodwinEgo <godwinehile04@gmail.com>
2023-08-02 23:39:46 +0100

    CONST

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,20 +3,32 @@ const { constants } = require("../constants");
 const errorHandler = (err, req, res, next) => {
   const statusCode = res ? res.statusCode : 500;
   switch (statusCode) {
-    case 400:
+    case constants.VALIDATION_ERROR:
       res.json({
         title: "Validation Failed",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case 404:
+    case constants.NOT_FOUND:
       res.json({
         title: "Not Found",
         message: err.message,
         stackTrace: err.stack,
       });
-
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.FORBIDDEN:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
     default:
       break;
   }
