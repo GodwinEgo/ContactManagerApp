024a6a1613c949d9d392b658b40d4cdef8310f21
GodwinEgo <godwinehile04@gmail.com>
2023-08-03 01:18:52 +0100

    db connect

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const connect = await mongoose.connect(process.env.CONNECTION_STRIG);
+    console.log(
+      "Database Connected: ",
+      connect.connection.host,
+      connect.connection.name
+    );
   } catch (err) {
     console.log(err);
     process.exit(1);
